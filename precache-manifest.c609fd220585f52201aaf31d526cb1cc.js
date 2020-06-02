99953cece40fca07f1bd77f1a54d6cb11b68dc41
LIN YEN　CHENG <clotha04976@gmail.com>
2020-06-02 22:54:02 +0800

    Updates

---
+++ precache-manifest.c609fd220585f52201aaf31d526cb1cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126c68d10d23ec73bb87ff151a57995b",
+    "url": "/ReactWebsite/index.html"
+  },
+  {
+    "revision": "1ae02b6b6fb0c0c897fb",
+    "url": "/ReactWebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65969750947a715475e0",
+    "url": "/ReactWebsite/static/js/2.2e06293d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWebsite/static/js/2.2e06293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae02b6b6fb0c0c897fb",
+    "url": "/ReactWebsite/static/js/main.edb6f3c7.chunk.js"
+  },
+  {
+    "revision": "0da4ee81bb0a685f3ffd",
+    "url": "/ReactWebsite/static/js/runtime-main.9b06ab9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
